c434e8ef8cb9ee87bf5023a6061ec66e478bed7e
Armin Roșu <git@armin.ro>
2021-08-13 09:15:47 +0200

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// @see http://json.schemastore.org/prettierrc
+
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+}
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   // don't pass staged files as an argument to this script
   '*': () => [
+    'yarn prettier --loglevel silent --write .',
     'yarn build',
-    'git add index.html'
-  ]
+    'git add index.html',
+  ],
 }
