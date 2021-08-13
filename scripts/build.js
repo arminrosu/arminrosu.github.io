f069a38fcebe995df657f857b11591bb966026f7
Armin Roșu <git@armin.ro>
2021-08-13 09:29:06 +0200

    Move files into subdirectories

--- scripts/build.js
+++ scripts/build.js
@@ -2,18 +2,22 @@ const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const fs = require('fs')
 const htmlmin = require('html-minifier').minify
+const path = require('path')
 const postcss = require('postcss')
 const sass = require('sass')
 
 /**
  * Compile HTML
- * @param  {string} css
+ * @param {string} css
  */
 const renderHtml = (css) => {
-  const banner = fs.readFileSync('html/_banner.html').toString().trim()
+  const banner = fs
+    .readFileSync(path.resolve(__dirname, '../src/html/_banner.html'))
+    .toString()
+    .trim()
 
   const html = fs
-    .readFileSync('html/index.html')
+    .readFileSync(path.resolve(__dirname, '../src/html/index.html'))
     .toString()
     .replace('<style />', '<style>' + css + '</style>')
 
@@ -26,12 +30,16 @@ const renderHtml = (css) => {
     removeRedundantAttributes: true,
   })
 
-  fs.writeFileSync('index.html', banner + minHtml)
+  fs.writeFileSync(
+    path.resolve(__dirname, '../docs/index.html'),
+    banner + minHtml
+  )
 }
 
 // Compile CSS
-
-const css = sass.renderSync({ file: 'scss/styles.scss' }).css.toString()
+const css = sass
+  .renderSync({ file: path.resolve(__dirname, '../src/scss/styles.scss') })
+  .css.toString()
 
 postcss([autoprefixer({ cascade: false }), cssnano])
   .process(css)
