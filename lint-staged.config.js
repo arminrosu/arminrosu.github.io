213f85162441b9f8120a523ee1176164c5cda54e
Armin Roșu <git@armin.ro>
2022-04-20 21:08:39 +0200

    Upgrade dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   // don't pass staged files as an argument to this script
   '*': () => [
+    'yarn lint:js --fix',
+    'yarn lint:styles --fix',
     'yarn prettier --loglevel silent --write .',
     'yarn build',
     'git add .',
