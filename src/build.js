00d89b6ca7c8122e8e69dcbcee9a6ec7314c6c0c
Armin Roșu <git@armin.ro>
2022-10-03 15:38:59 +0200

    Use markdown

--- src/build.js
+++ src/build.js
@@ -1,28 +1,43 @@
 import autoprefixer from 'autoprefixer'
+import MarkdownIt from 'markdown-it'
+import mia from 'markdown-it-abbr'
+import mila from 'markdown-it-link-attributes'
+import miis from 'markdown-it-imsize'
 import cssnano from 'cssnano'
 import { readFile, writeFile } from 'fs/promises'
 import { minify as htmlmin } from 'html-minifier'
 import postcss from 'postcss'
 import sass from 'sass'
 
+async function compileCSS() {
+  const css = sass.compile('./src/scss/styles.scss').css
+
+  return postcss([autoprefixer({ cascade: false }), cssnano]).process(css, {
+    from: undefined,
+  })
+}
+
 /**
  * Compile HTML
- * @param {string} css
+ * @param {string} html - Markdown compiled HTML.
+ * @param {string} css - SASS compiled CSS.
  */
-async function compileHTML(css) {
+async function compileHTML(html, css) {
   const banner = (
     await readFile('./src/html/_banner.html', {
       encoding: 'utf-8',
     })
   ).trim()
 
-  const html = (
+  const htmlTemplate = (
     await readFile('./src/html/index.html', {
       encoding: 'utf-8',
     })
-  ).replace('<style />', '<style>' + css + '</style>')
+  )
+    .replace('<style />', '<style>' + css + '</style>')
+    .replace('<body />', '<body>' + html + '</body>')
 
-  const minHtml = htmlmin(html, {
+  const minHtml = htmlmin(htmlTemplate, {
     collapseBooleanAttributes: true,
     collapseWhitespace: true,
     minifyJS: true,
@@ -36,17 +51,32 @@ async function compileHTML(css) {
   })
 }
 
-async function compileCSS() {
-  const css = sass.compile('./src/scss/styles.scss').css
+async function compileMarkdown() {
+  const markdown = (
+    await readFile('./src/markdown/index.md', {
+      encoding: 'utf-8',
+    })
+  )
+    // Prettier comments-out abbreviations: https://github.com/prettier/prettier/issues/5686
+    .replace(/\\\*\[/g, '*[')
 
-  return postcss([autoprefixer({ cascade: false }), cssnano]).process(css, {
-    from: undefined,
+  const md = new MarkdownIt({})
+  md.use(mia)
+  md.use(mila, {
+    attrs: {
+      target: '_blank',
+      rel: 'noopener',
+    },
   })
+  md.use(miis)
+
+  return md.render(markdown)
 }
 
 async function build() {
+  const html = await compileMarkdown()
   const css = await compileCSS()
-  await compileHTML(css)
+  await compileHTML(html, css)
 }
 
 build()
---
+++ src/markdown/index.md
@@ -0,0 +1,13 @@
+\*[RBF]: Resting Bitch Face
+
+# Armin Roșu
+
+Hi, I'm a developer, I build software and teams - since 2004™. I love what I do and hope you do too.
+
+You can find my [CV on LinkedIn](https://www.linkedin.com/in/arminrosu "Armin's public LinkedIn profile") and my [public projects on Github](https://github.com/arminrosu "Armin's Github profile").
+
+Most of the [technologies I've ever used are on Stackshare](https://stackshare.io/arminrosu/all "Armin's Stackshare"). You will find the most recent selection in my CV, but I included these to give you a feeling of my experience. Some of them are so odd or old, we'll have a chuckle talking about them.
+
+![Headshot of Armin Roșu](https://armin.ro/images/2016.07.jpg =1280x1280)
+
+On a personal note, I suffer from [RBF](https://en.wikipedia.org/wiki/Resting_bitch_face 'Resting Bitch Face on Wikipedia'), speak 4+ languages, have designed [watch](https://twitter.com/vector_watch/status/689477550037401600) [faces](https://codepen.io/arminrosu/pen/zrEymr), helped [break Enigma codes](http://www.enigmaathome.net/), funded [space travel](https://www.kickstarter.com/projects/michaellaine/space-elevator-science-climb-to-the-sky-a-tethered), am a Baron of the [Principality of Sealand](https://www.sealandgov.org/), have been [LEGO™ Ambassador to Romania](http://www.brothers-brick.com/2010/12/11/2011-lego-ambassadors-announced-news/), was featured in [Forbes](http://www.forbes.ro/articles/portret-de-trendsetter-armin-rosu_0_9205-9658) and was a [film editor](https://youtu.be/QMgzjOcT65s) once.
